6566049517c3d683e62d814c9d03c7837b445783
Pappu19 <58678139+Pappu19@users.noreply.github.com>
2020-08-03 16:47:08 +0600

    install pyautogui for auto working of mask

--- Webcam.py
+++ Webcam.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
+import pyautogui
 
 capture = cv2.VideoCapture(0)
 
 red_lower = np.array([136, 87, 111])
 red_upper = np.array([180, 255, 255])
+prev_y = 0
 
 while True:
     rate, frame = capture.read()
@@ -12,12 +14,16 @@ while True:
     mask = cv2.inRange(hsv, red_lower, red_upper)
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-
     for c in contours:
         area = cv2.contourArea(c)
         if area > 300:
             x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            if y < prev_y:
+                pyautogui.press('space')
+
+            prev_y = y
 
     cv2.imshow('Mask', mask)
     cv2.imshow("Frame", frame)
