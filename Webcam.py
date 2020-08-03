fc89bc923ebf466cbef1eb640638ed8b28f7bbe9
Pappu19 <58678139+Pappu19@users.noreply.github.com>
2020-08-03 16:19:36 +0600

    create border

--- Webcam.py
+++ Webcam.py
@@ -10,6 +10,8 @@ while True:
     rate, frame = capture.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, red_lower, red_upper)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
     cv2.imshow('Mask', mask)
     cv2.imshow("Frame", frame)
 
