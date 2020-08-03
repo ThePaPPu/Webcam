3661132fb92eeb13e47b0ad2531ba537584fcd44
Pappu19 <58678139+Pappu19@users.noreply.github.com>
2020-08-03 15:47:00 +0600

    Open webcam

--- Webcam.py
+++ Webcam.py
@@ -1,1 +1,15 @@
+from functools import cached_property
+
 import cv2
+
+capture = cv2.VideoCapture(0)
+
+while True:
+    rate, frame = capture.read()
+    cv2.imshow("Frame", frame)
+
+    if cv2.waitKey(10) == ord('c'):
+        break
+
+capture.release()
+cv2.destroyWindow()
